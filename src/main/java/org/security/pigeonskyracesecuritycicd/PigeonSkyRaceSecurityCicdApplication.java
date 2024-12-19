83e330444a1c23a5a9099a5379ac1d5e851d44b9
Ilyas chaoui <144786151+ichaoui56@users.noreply.github.com>
2024-12-19 10:08:04 +0100

    init commit

---
+++ src/main/java/org/security/pigeonskyracesecuritycicd/PigeonSkyRaceSecurityCicdApplication.java
@@ -0,0 +1,13 @@
+package org.security.pigeonskyracesecuritycicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PigeonSkyRaceSecurityCicdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PigeonSkyRaceSecurityCicdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/security/pigeonskyracesecuritycicd/PigeonSkyRaceSecurityCicdApplicationTests.java
@@ -0,0 +1,13 @@
+package org.security.pigeonskyracesecuritycicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PigeonSkyRaceSecurityCicdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
